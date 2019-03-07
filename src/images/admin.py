f8fefd38489a7a93bb8d4afd4930f4ace3abbd7e
Nishant Ranjan <darkfist1998@gmail.com>
2019-03-08 00:29:15 +0530

    Fixed some major bugs

--- src/images/admin.py
+++ src/images/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Image
+from .models import Image, ImageUpload
 
 admin.site.register(Image)
+admin.site.register(ImageUpload)
